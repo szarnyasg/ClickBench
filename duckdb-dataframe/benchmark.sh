1fd9fadf66e40446837ec21672cefc634ff9568c
Gabor Szarnyas <szarnyasg@gmail.com>
2024-11-27 08:19:54 +0100

    DuckDB Dataframe: cleanup script

--- duckdb-dataframe/benchmark.sh
+++ duckdb-dataframe/benchmark.sh
@@ -4,11 +4,11 @@
 
 sudo apt-get update
 sudo apt-get install -y python3-pip
-pip install --break-system-packages pandas duckdb
+pip install --break-system-packages pandas duckdb==1.1.3
 
 # Download the data
 wget --no-verbose --continue https://datasets.clickhouse.com/hits_compatible/athena/hits.parquet
 
 # Run the queries
 
-./run.sh 2>&1 | tee log.txt
+./run.sh | tee log.txt 2>&1
